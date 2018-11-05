91f9dd2df9118208143d98b78d14a1e860a037bb
bengrunfeld <benjamin.grunfeld@gmail.com>
2018-11-05 11:47:43 +1100

    Complete versatile bar chart

--- v4/02-bar-chart/main.js
+++ v4/02-bar-chart/main.js
@@ -1,33 +1,38 @@
 let svg = d3.select('.main').append('svg')
 
-let dataArr = [8,2,1,5,7,9,3,2,1,6,3,7,9,2,1,8]
+let dataArr = [8,2,1,5,30,9,3,2,15,6,3,7,9,2,1,25,22,15,6,3,7,9,]
+let [dataMin, dataMax] = d3.extent(dataArr)
+
+let margin = {top: 40, right: 0, bottom: 0, left: 30}
+let padding = 5
+let dimensions = {chartWidth: 0, chartHeight: (dataMax * 10), barWidth: 20, barHeight: 10}
 
 let xScale = d3.scaleLinear()
               .domain([0, dataArr.length])
-              .range([0, dataArr.length * 25])
+              .range([0, dataArr.length * (dimensions.barWidth + padding)])
 
 let yScale = d3.scaleLinear()
               .domain([0, Math.max(...dataArr)])
-              .range([120, 30])
+              .range([dimensions.chartHeight, 0])
 
-let xAxis = d3.axisBottom(xScale)
-let yAxis = d3.axisLeft(yScale)
-
-svg.append('g')
-  .attr("transform", `translate(20, 40)`)
-  .selectAll('rect').data(dataArr).enter().append('rect')
-    .attr('width', '20')
-    .attr('height', (d, i) => d * 10)
-    .attr('x', (d, i) => i * 25)
-    .attr('y', (d, i) => 100 - (d * 10))
-    .style('fill', 'steelblue')
+let xAxis = d3.axisBottom(xScale).ticks(3)
+let yAxis = d3.axisLeft(yScale).ticks(3)
 
 svg.append('g')
   .call(xAxis)
-  .attr("transform", `translate(20, 140)`)
+  .attr("transform", `translate(${margin.left}, ${margin.top + dimensions.chartHeight + padding})`)
 
 svg.append('g')
   .call(yAxis)
-  .attr("transform", `translate(20, 20)`)
+  .attr("transform", `translate(${margin.left - 5}, ${margin.top})`)
+
+svg.append('g')
+  .attr("transform", `translate(${margin.left}, ${margin.top})`)
+  .selectAll('rect').data(dataArr).enter().append('rect')
+    .attr('width', dimensions.barWidth)
+    .attr('height', (d, i) => d * dimensions.barHeight)
+    .attr('x', (d, i) => i * (dimensions.barWidth + padding))
+    .attr('y', (d, i) => dimensions.chartHeight - (d * dimensions.barHeight))
+    .style('fill', 'steelblue')
 
 
