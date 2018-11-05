1a36e3261189dd3c668088f5d37673d4ff8b4046
bengrunfeld <benjamin.grunfeld@gmail.com>
2018-11-05 14:17:39 +1100

    Complete Japanese Candlestick chart without tooltip

--- v5/03-japanese-candlesticks/main.js
+++ v5/03-japanese-candlesticks/main.js
@@ -13,38 +13,53 @@ let dataMin = dataArr.reduce((a, b) => (a.l < b.l) ? a : b)
 
 let margin = {left: 30, top: 30, right: 0, bottom: 0}
 let padding = 5
-let dimensions = {chartWidth: 0, chartHeight: 200, barWidth: 10, barHeight: 0}
+let dimensions = {chartWidth: 0, chartHeight: 200, candleWidth: 9, barHeight: 0, wickWidth: 1}
 
 let xScale = d3.scaleLinear()
               .domain([0, dataArr.length])
-              .range([0, dataArr.length * (dimensions.barWidth + padding)])
+              .range([0, dataArr.length * (dimensions.candleWidth + padding)])
 
 let yScale = d3.scaleLinear()
               .domain([dataMin.l, dataMax.h])
               .range([dimensions.chartHeight, 0])
 
 let xAxis = d3.axisBottom(xScale).ticks(3)
-let yAxis = d3.axisLeft(yScale).ticks(3)
+let yAxis = d3.axisLeft(yScale)//.ticks(3)
 
 let svg = d3.select('.main').append('svg')
 
 svg.append('g')
   .call(xAxis)
-  .attr('transform', `translate(${margin.left}, ${dimensions.chartHeight + padding})`)
+  .attr('transform', `translate(${margin.left}, ${dimensions.chartHeight + (padding * 2)})`)
 
 svg.append('g')
   .call(yAxis)
   .attr('transform', `translate(${margin.left - padding}, ${padding})`)
 
+
+// Open and Close
 svg.append('g')
   .attr('transform', `translate(${margin.left}, ${padding})`)
   .selectAll('rect').data(dataArr).enter().append('rect')
-    .attr('width', dimensions.barWidth)    
-    .attr('height', (d, i) => (d.c > d.o) ? (d.c - d.o) : (d.o - d.c))
-    .attr('x', (d, i) => i * (dimensions.barWidth + padding))
+    .attr('width', dimensions.candleWidth)    
+    .attr('height', (d, i) => {
+      const height = (d.c > d.o) ? yScale(d.o) - yScale(d.c) : yScale(d.c) - yScale(d.o)
+      return (height !== 0) ? height : 1
+    })
+    .attr('x', (d, i) => i * (dimensions.candleWidth + padding))
     .attr('y', (d, i) => (d.c > d.o) ? yScale(d.c) : yScale(d.o))
     .style('fill', (d, i) => (d.c > d.o) ? 'green' : 'red')
 
+
+// High and Low
+svg.append('g')
+  .attr('transform', `translate(${margin.left}, ${padding})`)
+  .selectAll('rect').data(dataArr).enter().append('rect')
+    .attr('width', dimensions.wickWidth)    
+    .attr('height', (d, i) => yScale(d.l) - yScale(d.h))
+    .attr('x', (d, i) => i * (dimensions.candleWidth + padding) + (Math.floor(dimensions.candleWidth / 2)))
+    .attr('y', (d, i) => yScale(d.h))
+    .style('fill', (d, i) => (d.c > d.o) ? 'green' : 'red')
 
 
 
